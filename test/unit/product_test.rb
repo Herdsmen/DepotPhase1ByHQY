2f885bac8f60237f9b4685d728e1792e3f1da9dc
HuangQiyuan <122253922@qq.com>
2011-07-13 14:14:33 +0800

    use Fixture

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -50,4 +50,13 @@ class ProductTest < ActiveSupport::TestCase
       assert new_product(name).invalid?, "#{name} shouldn't be valid"
     end
   end
+  
+  test "product is not valid without a unique title - i18n" do
+    product = Product.new(:title        => products(:ruby).title,
+                            :description  => "yyy",
+                            :price        => 1,
+                            :image_url    => "fred.gif")
+    assert !product.save
+    assert_equal I18n.translate('activerecord.errors.messages.taken'), product.errors[:title].join(';')
+  end
 end
